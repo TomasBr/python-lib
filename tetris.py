643be693330e56ab90c7c12c4f70e9916ade8e5b
Tomas Brännström <Tomas@Tomas-iMac.local>
2020-07-24 17:31:57 +0200

    Handle user input

--- tetris.py
+++ tetris.py
@@ -8,6 +8,8 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 FPS = 60
+CAPTION_FONT_NAME = "comicsans"
+CAPTION_FONT_SIZE = 60
 
 s_width = 640
 s_height = 480
@@ -21,16 +23,29 @@ top_left_y = s_height - play_height - 10
 
 
 def main():
-    """ game entry point """
-    global screen
+    global screen, clock, caption_font
     pygame.init()
-
     # Set up the drawing window
     screen = pygame.display.set_mode([s_width, s_height])
     pygame.display.set_caption("Tetris")
     clock = pygame.time.Clock()
-    font = pygame.font.SysFont('comicsans', 60)
-    label = font.render('TETRIS', 1, WHITE)
+    caption_font = pygame.font.SysFont(CAPTION_FONT_NAME, CAPTION_FONT_SIZE)
+    run_game()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+    pygame.quit()
+
+def run_game():
+    """ game entry point """
+    global caption_label
+
+    caption_label = make_text_object("TETRIS", caption_font, WHITE)
     model = game_model(board_width, board_height)
 
     # Run until the user asks to quit
@@ -43,6 +58,28 @@ def main():
         if falling_block is None:
             falling_block = get_new_block()
             last_fall_time = time.time()
+            if not is_valid_position(model, falling_block, adj_y=1):
+                return
+
+        # Handle user keyboard events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT and is_valid_position(model, falling_block, adj_x=-1):
+                    falling_block.x -= 1
+                elif event.key == pygame.K_RIGHT and is_valid_position(model, falling_block, adj_x=1):
+                    falling_block.x += 1
+                elif event.key == pygame.K_UP:
+                    print("Up arrow")
+                elif event.key == pygame.K_DOWN and is_valid_position(model, falling_block, adj_y=1):
+                    falling_block.y += 1
+                    last_fall_time = time.time()
+                elif event.key == pygame.K_SPACE:
+                    for y in range(1, board_height):
+                        if not is_valid_position(model, falling_block, adj_y=y):
+                            break
+                    falling_block.y += y - 1
 
         now = time.time()
         if now - last_fall_time >= fall_speed:
@@ -53,32 +90,11 @@ def main():
                 model.update(falling_block)
                 falling_block = None
 
-        # Handle user keyboard events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    falling_block.x -= 1
-                elif event.key == pygame.K_RIGHT:
-                    falling_block.x += 1
-                elif event.key == pygame.K_UP:
-                    print("Up arrow")
-                elif event.key == pygame.K_DOWN:
-                    falling_block.x += 1
-                elif event.key == pygame.K_SPACE:
-                    print("Space key")
-
-        # Fill the background with black
-        screen.fill(BLACK)
-        screen.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), 25))
-        pygame.draw.rect(screen, (94, 18, 36), (top_left_x, top_left_y, play_width, play_height), 5)
-
         draw_board(model)
         if falling_block is not None:
             draw_block(falling_block)
 
-        # Flip the display
+        # Update the display
         pygame.display.update()
         clock.tick(FPS)
     # Done! Time to quit.
@@ -89,7 +105,7 @@ def is_on_board(x, y):
     return 0 <= x < board_width and y < board_height
 
 
-def is_valid_position(model, block, adj_x = 0, adj_y = 0):
+def is_valid_position(model, block, adj_x=0, adj_y=0):
     for x in range(len(block.block_type[0])):
         for y in range(len(block.block_type)):
             if not is_on_board(x + block.x + adj_x, y + block.y + adj_y):
@@ -98,7 +114,12 @@ def is_valid_position(model, block, adj_x = 0, adj_y = 0):
                 return False
     return True
 
+
 def draw_board(model):
+    screen.fill(BLACK)
+    screen.blit(caption_label, (top_left_x + play_width / 2 - (caption_label.get_width() / 2), 25))
+    pygame.draw.rect(screen, (94, 18, 36), (top_left_x, top_left_y, play_width, play_height), 5)
+
     for x in range(model.width):
         for y in range(model.height):
             if model.landed_blocks[x][y] != 0:
@@ -123,6 +144,10 @@ def convert_to_pixel_coords(box_x, box_y):
 
 def get_new_block():
     return block(board_width, -1)
+
+
+def make_text_object(text, font, color):
+    return font.render(text, True, color)
 
 
 if __name__ == "__main__":
